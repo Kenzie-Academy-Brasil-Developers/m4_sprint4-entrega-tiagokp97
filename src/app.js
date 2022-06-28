c79baf334abc3a5c30cf8c1557525b184c624355
tiagokp97 <tiagopesch@gmail.com>
2022-06-28 19:29:37 -0300

    Entrega

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
-import express from "express";
 import "dotenv/config";
+import { startDatabase } from "./database";
+import express from "express";
+import productsRouter from "./routes/products.routes";
+import categoriesRouter from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/categories", categoriesRouter);
+app.use("/products", productsRouter);
 
-export default app.listen(3333, () => {
-  console.log("Server running");
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World Kenzie",
+  });
 });
+
+export default app.listen(process.env.PORT, () => {
+  console.log(`Server running in port 3000!`);
+  startDatabase();
+});
+
+// app.listen(process.env.ENV === "test" ? 3001 : 3000, () => {
+//   console.log("Executando na porta 3000");
+//   startDatabase();
+// });
+
+// export default app;
--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,14 @@
 import { Client } from "pg";
+import "dotenv/config";
 
 const database = new Client(
   process.env.NODE_ENV === "test"
     ? {
-        user: "",
-        host: "",
-        database: "",
-        password: "",
-        port: 5432,
+        host: "localhost",
+        port: 5431,
+        user: "tiago",
+        database: "products_management",
+        password: "123",
       }
     : {
         user: process.env.DB_USER,
@@ -20,6 +21,7 @@ const database = new Client(
 
 export const startDatabase = async () => {
   await database.connect();
+  console.log(`Conectado com sucesso ao banco de dados!`);
 };
 
 export default database;
